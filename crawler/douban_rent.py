e79940c62b0cf3d87edc00c085ec3917fe62e2ae
levin <levin@theplant.jp>
2023-08-13 16:41:34 +0800

    feat: add douban group rent info crawler

--- crawler/douban_rent.py
+++ crawler/douban_rent.py
@@ -200,7 +200,8 @@ def randomSleep():
     time.sleep(num)
 
 def getCookie():
-    return ""
+    return """"""
+
 def getHeader(cookie):
     headers = {
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
